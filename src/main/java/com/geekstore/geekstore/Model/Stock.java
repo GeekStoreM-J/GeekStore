18423b97c7c752b05882948d0852c359e5bbfae9
MarcosKauanS <kauanplg2020@gmail.com>
2025-10-22 12:20:12 -0300

    feat(database): configura MySQL e criar models User, Product e Stock

---
+++ src/main/java/com/geekstore/geekstore/Model/Stock.java
@@ -0,0 +1,42 @@
+package com.geekstore.geekstore.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "stocks")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
